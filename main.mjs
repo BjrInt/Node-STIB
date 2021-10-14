b1306f86656971c879df91313292b1bb9a610dcd
Bonjour Internet <kelian@sailsense.io>
2021-10-14 16:03:45 +0200

    [add] Stops query + cleanup

--- main.mjs
+++ main.mjs
@@ -1,30 +1,27 @@
 import { 
   httpQuery,
   splitInSubgroupOf,
-  Throttler 
 } from './helpers.mjs'
 import { 
-  DEFAULT_OPTIONS,
   ENDPOINTS,
-  LIMIT_QUERY_PARAM,
+  LIMIT_QUERY_PARAM,  
 } from './constants.mjs'
+import { RATE_LIMITS } from './constants.mjs'
 
 class Stib{
-  constructor(token, options={}){
+  constructor(token){
     this.token = token
-    this.options = {
-      ...DEFAULT_OPTIONS,
-      ...options
-    }
-    this.throttler = Throttler()
   }
 
-  queryLines(lines){
+  getLinesDescription(lines){
     return new Promise((resolve, reject) => {
+      if(lines.length > LIMIT_QUERY_PARAM.NETWORK_DESCRIPTION.LINES * RATE_LIMITS.NETWORK_DESCRIPTION)
+        return reject({error: 'RATE_LIMIT_EXCEEDED'})
+      
       const queries = splitInSubgroupOf(lines, LIMIT_QUERY_PARAM.NETWORK_DESCRIPTION.LINES)
                       .map(q => httpQuery(this.token, ENDPOINTS.NETWORK_DESCRIPTION.LINES, q))
       
-      Promise.all(queries)
+      return Promise.all(queries)
              .then((data) => (
                 resolve( data.map(d => d.lines).flat() ) 
               ))
@@ -32,8 +29,20 @@ class Stib{
     })
   }
 
-  queryStops(){
+  getStopsDescription(stops){
+    return new Promise((resolve, reject) => {
+      if(stops.length > LIMIT_QUERY_PARAM.NETWORK_DESCRIPTION.STOPS * RATE_LIMITS.NETWORK_DESCRIPTION)
+        return reject({error: 'RATE_LIMIT_EXCEEDED'})
+      
+      const queries = splitInSubgroupOf(stops, LIMIT_QUERY_PARAM.NETWORK_DESCRIPTION.STOPS)
+                      .map(q => httpQuery(this.token, ENDPOINTS.NETWORK_DESCRIPTION.STOPS, q))
 
+      return Promise.all(queries)
+             .then((data) => (
+                resolve( data.map(d => d.points).flat() ) 
+              ))
+              .catch(err => reject(err))
+    })
   }
 }
 
